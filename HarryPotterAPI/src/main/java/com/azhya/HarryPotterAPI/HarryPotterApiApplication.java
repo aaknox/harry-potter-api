b7b050681fd3830b4a00a39fe97761c68e3b09ed
Azhya Knox <azhya.knox@revature.com>
2021-07-25 14:01:30 -0500

    added starter spring project

---
+++ HarryPotterAPI/src/main/java/com/azhya/HarryPotterAPI/HarryPotterApiApplication.java
@@ -0,0 +1,13 @@
+package com.azhya.HarryPotterAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarryPotterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarryPotterApiApplication.class, args);
+	}
+
+}
---
+++ HarryPotterAPI/src/test/java/com/azhya/HarryPotterAPI/HarryPotterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azhya.HarryPotterAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarryPotterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
