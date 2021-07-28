fd340da7a62d875843e529c6dc441b3a1e2d6581
Azhya Knox <azhya.knox@revature.com>
2021-07-27 23:03:55 -0500

    testing sonarcloud analysis

--- HarryPotterAPI/src/main/java/com/azhya/HarryPotterAPI/HarryPotterApiApplication.java
+++ HarryPotterAPI/src/main/java/com/azhya/HarryPotterAPI/HarryPotterApiApplication.java
@@ -3,6 +3,12 @@ package com.azhya.HarryPotterAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This service will deal with all of the API data requests and manpulating it for my own services.
+ * 
+ * 
+ * @author Azhya Knox
+ **/
 @SpringBootApplication
 public class HarryPotterApiApplication {
 
